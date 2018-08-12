741125c2763677563111e8816aff5a1df85245ae
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-08-11 21:10:27 -0400

    first lecture, interval scheduling

---
+++ lecture01.py
@@ -0,0 +1,98 @@
+"""
+Interval Scheduling
+
+"""
+
+
+class Request:
+  """
+  Request
+
+  start: int, assume >= 0
+  finish: int, assume > 0
+
+  """
+
+  def __init__(self, start, finish):
+    self.start = start
+    self.finish = finish
+
+  def is_compatible(self, req):
+    return self.finish <= req.start \
+      or self.start >= req.finish
+
+
+def greedy_largest_compatible_subset(requests):
+  """
+  Given requests for a single resource
+  and a list of requests
+
+  We want to find the largest subset
+  of compatible requests
+
+  A request is compatible if
+  r1.finish <= r2.start or r1.start >= r2.finish
+
+  Greedy algorithm:
+  - Pick request with earliest finish time, r
+  - Add r to the result
+  - Filter out all remaining requests incompatible with r
+
+  requests: List[int]
+  start: Dict[int]int
+  finish: Dict[int]int
+
+  """
+  result = []
+  while requests:
+    best = min(requests, key=lambda r: r.finish)
+    result.append(best)
+    requests = filter(
+      lambda r: r != best and r.is_compatible(best), requests)
+  return result
+
+
+class WeightedRequest(Request):
+  """
+  WeightedRequest
+
+  weight: int
+
+  """
+
+  def __init__(self, start, finish, weight):
+    Request.__init__(self, start, finish)
+    self.weight = weight
+
+
+def optimal_compatible_subset(requests):
+  """
+  Get optimal weighted subset
+
+  Uses dynamic programming to get the subset
+  of the requests with the largest weight
+
+  """
+  dp_weights = dict()
+  dp_subsets = dict()
+  finishes = {r.finish for r in requests}
+  while finishes:
+    x = max(finishes)
+    finishes.remove(x)
+    subsets = dict()
+    for req in [r for r in requests if r.start >= x]:
+      w = req.weight + dp_weights[req.finish]
+      subsets[w] = [req] + dp_subsets[req.finish]
+    if not subsets:
+      dp_weights[x] = 0
+      dp_subsets[x] = []
+    else:
+      w = max(subsets)
+      dp_weights[x] = w
+      dp_subsets[x] = subsets[w]
+  for req in [r for r in requests if r.start < min(dp_weights)]:
+    w = req.weight + dp_weights[req.finish]
+    if 0 not in dp_weights or w > dp_weights[0]:
+      dp_weights[0] = w
+      dp_subsets[0] = [req] + dp_subsets[req.finish]
+  return (dp_weights[0], dp_subsets[0])

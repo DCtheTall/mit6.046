fbb0ede95f0c933bf9fd6c49fcb69bfe04be2d4e
DCtheTall <dylan.james.cutler@gmail.com>
2019-01-02 01:16:38 -0500

    service object for knapsack

--- lecture22/knapsack.py
+++ lecture22/knapsack.py
@@ -52,3 +52,107 @@ message to 1. Traverse W until c' is reduced to 0, and the
 resulting 32 bits is the message Bob sent.
 
 """
+
+
+from random import randint
+
+
+def gcd(a, b):
+  """
+  Compute GCD of two integers.
+
+  """
+  a, b = max(a, b), min(a, b)
+  while b != 0:
+    a, b = b, a % b
+  return a
+
+
+def mod_inverse(x, n):
+  """
+  Compute the inverse of x in the multiplicative
+  group of Z/nZ, i.e. the integer y such that
+
+  x * y = 1 mod N.
+
+  The algorithm uses the extended Euclidean
+  algorithm to find the inverse efficiently.
+
+  """
+  a, b = n, x
+  ta, tb = 0, 1
+  while b != 0:
+    q = a / b
+    a, b = b, a % b
+    ta, tb = tb, ta - (q * tb)
+  if ta < 0:
+    ta += n
+  return ta
+
+
+class Service(object):
+  """
+  Service object represents a server or process that is
+  using the knapsack cyptosystem to exchange encrypted
+  32-bit unsigned integers with another Service instance.
+
+  """
+  def __init__(self):
+    cur_sum = 0
+    self.W = [] # private key
+    for _ in range(32):
+      cur_sum += randint(1, 100)
+      self.W.append(cur_sum)
+    self.q = randint(cur_sum + 1, 2 * cur_sum)
+    self.r = randint(1, 2 * q)
+    while gcd(self.r, self.q) != 1:
+      self.r = randint(1, 2 * q)
+    self.B = [(self.r * w) % self.q for w in self.W] # public key
+    self.s = mod_inverse(self.r, self.q)
+    self.dst_B = None # partner's public key
+    self.port = None
+
+  def encrypt_msg(self, msg):
+    """
+    Use the partner's public key to encrypt a message
+    (a 32 bit unsigned integer), into an encrypted
+    integer c.
+
+    """
+    c = 0
+    for i in range(32):
+      c += (msg & (1 << i)) * self.dst_B[i]
+    return c
+
+  def receive_key(self, B):
+    """
+    Receive the public key from the message
+    partner.
+
+    """
+    self.dst_B = B
+
+  def receive_msg(self, c):
+    """
+    Decrypt a message from another service
+    instance which was sent this service's public
+    key and used it to encrypt the ciphertext,
+    c.
+
+    """
+    c = (c * self.s) % self.q
+    msg = 0
+    for i, w in reversed(self.W):
+      if c >= w:
+        msg += (1 << (31 - i))
+        c -= w
+    print msg
+
+  def send_msg(self, msg):
+    """
+    Mock public method for sending an encrypted
+    message using this service to its partner,
+    triggering the receive_msg callback.
+
+    """
+    self.port.send_msg(self.encrypt_msg(msg))
